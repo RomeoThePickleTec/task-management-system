13b4d99d77ed7a6b1116c517e1ab580c69c492fb
Fercubos <fernandoyiuuu@gmail.com>
2025-05-13 12:34:11 -0600

    Encriptado_bien

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'cypress';
 import * as fs from 'fs';
 import CryptoJS from 'crypto-js';
+require('dotenv').config(); 
 
-const SECRET_KEY = '24)#^Of3qm62U$@BgHc*]VlJ.e7~SADz';
+const SECRET_KEY = process.env.SECRET_KEY || '24)#^Of3qm62U$@BgHc*]VlJ.e7~SADz';
 
 function decryptEnvVariables() {
   const envPath = './cypress.env.enc';
@@ -19,7 +20,7 @@ export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       const decryptedEnv = decryptEnvVariables();
-      config.env = { ...config.env, ...decryptedEnv };
+      config.env = { ...config.env, ...decryptedEnv, apiKey: process.env.CYPRESS_API_KEY };
       return config;
     },
     experimentalStudio: true,
